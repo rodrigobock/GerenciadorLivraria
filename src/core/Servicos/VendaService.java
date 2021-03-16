051bd8a15524899d91fb68fdaf32b09d883748c2
Erick Eduardo Baron <erick.baron@hbsis.com.br>
2021-03-16 12:47:20 -0300

    Correção do service

--- src/core/Servicos/VendaService.java
+++ src/core/Servicos/VendaService.java
@@ -8,7 +8,11 @@ import java.util.List;
 
 public class VendaService {
     private final ManipuladorArquivos manipuladorArquivos = new ManipuladorArquivos();
+    private final ProdutoUtils utils = new ProdutoUtils();
+
     private final List<String> TIPO_PRODUTOS = Arrays.asList(VendaEnum.PAPEL.name(), VendaEnum.LAPIS.name(),VendaEnum.CANETA.name(),VendaEnum.BORRACHA.name());
+    private final String TIPO_ACAO_VENDA = "venda";
+    private final String NOME_ARQUIVO_REGISTRO_VENDA = "registro_vendas.txt";
 
     public void Setup(){
         for (int i = 0; i < TIPO_PRODUTOS.size(); i++) {
@@ -18,7 +22,7 @@ public class VendaService {
         }
     }
     
-	public void CriaRegistro(){
+	private void CriaRegistro(){
 
 		if (!manipuladorArquivos.arquivoExiste("registro_vendas.txt"))
 		{
@@ -42,5 +46,64 @@ public class VendaService {
            System.out.println(produtos.get(i));
        }
        return produtos;
+    }
+
+    public boolean vendas(VendaEnum tipo, int qtde) {
+
+        CriaRegistro();
+
+        List<String> produtos = manipuladorArquivos.lerArquivo(tipo.name() + ".txt");
+
+        int qtdeItem = 0;
+        double valorProduto = 0;
+        Produto produto;
+
+        for(int i = 0; i < produtos.size(); i++){
+            produto = utils.setProduto(produtos.get(i));
+            qtdeItem = qtdeItem + produto.getQuantidade();
+            valorProduto = produto.getPreco();
+        }
+
+        double valorTotal = valorProduto * qtde;
+        if (qtde <= qtdeItem && qtde != 0) {
+
+            System.out.println("Compra realizada com sucesso!");
+
+            String conteudo = "Produto: " + tipo.name() +";\n Quantidade: " + qtde + "; Valor unitário: " + valorProduto + ";\n Valor total: " + valorTotal + ";\n";
+            manipuladorArquivos.escreveArquivo(NOME_ARQUIVO_REGISTRO_VENDA, conteudo);
+
+            VendaService service = new VendaService();
+            Produto pdt = new Produto();
+
+            pdt.setDescricao(TIPO_ACAO_VENDA);
+            pdt.setTipoProduto(tipo);
+            pdt.setQuantidade(-qtde);
+
+            service.RegistrarProduto(pdt);
+        } else if(qtde == 0){
+            System.out.println("Impossível realizar a compra com quantidade zerada!");
+        }
+        else {
+            System.out.println("Estoque insuficiente!");
+        }
+        return true;
+    }
+
+    public String listarEstoque(VendaEnum tipo){
+
+        List<String> produtos = manipuladorArquivos.lerArquivo(tipo.name() + ".txt");
+
+        int qtde = 0;
+        Produto produto;
+        String retorno;
+
+        for(int i = 0; i < produtos.size(); i++){
+            produto = utils.setProduto(produtos.get(i));
+            qtde = qtde + produto.getQuantidade();
+        }
+
+        retorno = "Quantidade total de " + tipo.name() + " = " + qtde;
+        System.out.println(retorno);
+        return retorno;
     }
 }
--- src/testes/Testes.java
+++ src/testes/Testes.java
@@ -2,9 +2,7 @@ package testes;
 
 import core.Entidades.Produto;
 import core.Entidades.VendaEnum;
-import core.Servicos.ControleEstoque;
 import core.Servicos.VendaService;
-import core.Servicos.Vendas;
 
 public class Testes {
 
@@ -12,34 +10,32 @@ public class Testes {
 				
 		VendaService service = new VendaService();
 		Produto produto = new Produto();
-		ControleEstoque estoque = new ControleEstoque();
-		Vendas vendas = new Vendas();		
-		
+
 		service.Setup();
 
-//		produto.setCodigo(121);
-//		produto.setTipoProduto(VendaEnum.BORRACHA);
-//		produto.setDescricao("123");
-//		produto.setMarca("saeads");
-//		// produto.setCor();
-//		produto.setTamanho("Grande");
-//		produto.setQuantidade(10);
-//		produto.setPreco(8.99);
-//
-//		service.RegistrarProduto(produto);
-//		service.RecuperarProdutos(VendaEnum.LAPIS);
-//		service.RecuperarProdutos(VendaEnum.BORRACHA);
-//
-//
-//
-//		System.out.println("Controle de estoque: ");
-//
-//		estoque.listarEstoque(VendaEnum.BORRACHA);
-//		estoque.listarEstoque(VendaEnum.LAPIS);
-//		estoque.listarEstoque(VendaEnum.PAPEL);
-//		estoque.listarEstoque(VendaEnum.CANETA);
+		produto.setCodigo(121);
+		produto.setTipoProduto(VendaEnum.BORRACHA);
+		produto.setDescricao("123");
+		produto.setMarca("saeads");
+		// produto.setCor();
+		produto.setTamanho("Grande");
+		produto.setQuantidade(10);
+		produto.setPreco(8.99);
 
-		vendas.vendas(VendaEnum.BORRACHA, 3);
-		estoque.listarEstoque(VendaEnum.BORRACHA);
+		service.RegistrarProduto(produto);
+		service.RecuperarProdutos(VendaEnum.LAPIS);
+		service.RecuperarProdutos(VendaEnum.BORRACHA);
+
+
+
+		System.out.println("Controle de estoque: ");
+
+		service.listarEstoque(VendaEnum.BORRACHA);
+		service.listarEstoque(VendaEnum.LAPIS);
+		service.listarEstoque(VendaEnum.PAPEL);
+		service.listarEstoque(VendaEnum.CANETA);
+
+		service.vendas(VendaEnum.BORRACHA, 3);
+		service.listarEstoque(VendaEnum.BORRACHA);
 	}
 }
